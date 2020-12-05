d34e204cf267577bcefe6cef732ccf886876890b
RastaPasta <pepperbel@hotmail.com>
2020-12-05 22:56:02 +0100

    day_5 part 1

--- advent_2020_alig8r/Scripts/day_5.py
+++ advent_2020_alig8r/Scripts/day_5.py
@@ -8,25 +8,60 @@ with open(os.path.join(sys.path[0], "../Inputs/input_day_5.txt"), "r") as my_inp
 
 _FB_RANGE = [0, 127]
 _LR_RANGE = [0, 7]
+_SEATS_TAKEN = {}
 
-def get_new_range(letter, range):
-    middle = math.floor(range[1] / 2)
-    if letter == "F":
-        range = [range[0], middle]
-    elif letter == "B":
-        range = [middle + 1, range[1]]
+def get_new_range(letter, curr_range):
+    middle = curr_range[0] + ((curr_range[1] - curr_range[0]) / 2)
+    if letter == "F" or letter == "L":
+        curr_range = [curr_range[0], math.floor(middle)]
+    elif letter == "B" or letter == "R":
+        curr_range = [math.ceil(middle), curr_range[1]]
     else:
         print("Invalid Letter")
-    return range
+    return curr_range
 
-#for entry in _INPUT_1:
-entry = "FBBBBFBRLL"
 
-new_range = _FB_RANGE
+def get_final_value(letter, curr_range):
+    if letter == "F" or letter == "L":
+        return curr_range[0]
+    if letter == "B" or letter == "R":
+        return curr_range[1]
 
-for letter in entry:
-    new_range = get_new_range(letter, new_range)
-    #print(new_range)
+
+_HIGHEST_ID = 0
+
+
+for entry in _INPUT_1:
+    #entry = "FBFBBFBRLL" 
+    _SEAT_ROW = 0
+    _SEAT_COLUMN = 0
+    _SEAT_ID = 0
+
+    #print("START: ", _SEAT_ID, _HIGHEST_ID)
+
+    new_range_fb = _FB_RANGE
+    new_range_lr = _LR_RANGE
+
+    entry = [entry[:7], entry[7:]]
+
+    for row in entry[0]:
+        new_range_fb = get_new_range(row, new_range_fb)
+    _SEAT_ROW = get_final_value(entry[0][-1], new_range_fb)
+    for column in entry[1]:
+        new_range_lr = get_new_range(column, new_range_lr)
+    _SEAT_COLUMN = get_final_value(entry[1][-1], new_range_lr)
+    _SEAT_ID = (_SEAT_ROW * 8) + _SEAT_COLUMN
+    _SEATS_TAKEN[_SEAT_ROW] = _SEAT_COLUMN
+
+    if _SEAT_ID > _HIGHEST_ID:
+        _HIGHEST_ID = _SEAT_ID
+
+    #print("END: ", _SEAT_ID, _HIGHEST_ID)
+
+print("FIN FIN: ", _HIGHEST_ID, _SEATS_TAKEN)
+
+
+
 
 
 
