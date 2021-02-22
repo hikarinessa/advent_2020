e183c0de653ca3b0dccef7632cd873e92170c7b0
kippchenburger <ludwigplath@gmail.com>
2021-02-22 22:50:38 +0100

    day 16 and 17 solve

---
+++ advent_2020_ludwig/AOC20/AOC20_16_traintickets.py
@@ -0,0 +1,185 @@
+# https://adventofcode.com/2020/day/16
+
+# Start by determining which tickets are completely invalid; these 
+# are tickets that contain values which aren't valid for any field. Ignore your ticket for now.
+# For example, suppose you have the following notes:
+
+data = """class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"""
+
+# It doesn't matter which position corresponds to which field; 
+# you can identify invalid nearby tickets by considering only 
+# whether tickets contain values that are not valid for any field. 
+# In this example, the values on the first nearby ticket are all 
+# valid for at least one field. This is not true of the other three 
+# nearby tickets: the values 4, 55, and 12 are are not valid for 
+# any field. Adding together all of the invalid values produces your 
+# ticket scanning error rate: 4 + 55 + 12 = 71.
+
+debug = False
+printit = False
+part2 = True
+
+if part2:
+    data = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"""
+
+import copy
+
+def pp(subject, name = "", override = False): #prints anything with a name as string 
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        print()
+        print(name, ": ", subject)
+
+
+def ppp(subject, name = "", override = False, isDictionary=False): #prints list entries
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        print()
+        print(name, ": ")
+        for item in subject:
+            if isDictionary:
+                print(item, ":", end="")
+                print(subject[item])
+            else:
+                print(item)
+
+        
+
+print("\n----------------------------------")   # reading file
+
+file = "advent_2020_ludwig/AOC20/data_aoc20_16.txt"
+if not debug:
+    with open(file) as f:
+        data = f.read()
+
+data = [s for s in data.split("\n\n")]
+rules = [s for s in data[0].split("\n")]
+# ppp(rules, "rules")
+myticket = [s.split(",") for s in data[1].split("\n")][1]
+pp(myticket, "my ticket")
+nearby_tickets = [[int(t) for t in s.split(",")] for s in data[2].split("\n")[1:]]
+# ppp(nearby_tickets, "nearby tickets")
+
+for index, line in enumerate(rules):
+    split = [s for s in line.split(": ")]
+    rules[index] = [split[0]]  
+    for pair in [s.split("-") for s in split[1].split(" or ")]:
+        pair = [int(x) for x in pair]
+        rules[index].append(pair) 
+ppp(rules, "rules", True)
+
+# ----- Part 1 ------
+
+invalids = []
+valid_tickets = []
+for ticket in nearby_tickets:
+    #pp(ticket, "ticket")
+    ticket_valid = True
+    for index, entry in enumerate(ticket):
+        strikes = 0
+        for rule in rules:
+            min1, max1, min2, max2 = rule[1][0], rule[1][1], rule[2][0], rule[2][1]
+            if entry not in range(min1, max1+1):
+                if entry not in range(min2, max2+1):
+                    strikes += 1
+        if strikes == len(rules):            
+            pp(entry, "invalid entry")
+            invalids.append(entry)
+            ticket_valid = False
+    if ticket_valid:
+        valid_tickets.append(ticket) # -- list of valid tickets for part 2
+
+error_rate = sum(invalids)
+pp(error_rate, "error rate", True)
+
+# ----- Part 2 -------
+
+#ppp(valid_tickets, "valid tickets", True)
+rule_positions = {}
+possible_positions = len(rules)
+number_of_tickets = len(valid_tickets)
+
+for rule in rules:
+    rule_positions[rule[0]] = []
+    #pp(rule, "rule", True)
+    min1, max1, min2, max2 = rule[1][0], rule[1][1], rule[2][0], rule[2][1]
+    position = 0
+    match_found = False
+    for position in range(possible_positions):
+        #pp(position, "--position to check", True)
+        matches = 0
+        
+        for ticket in valid_tickets:
+            entry = ticket[position]
+            if entry in range(min1, max1+1) or entry in range(min2, max2+1):
+                matches += 1
+
+        if matches == number_of_tickets:
+            rule_positions[rule[0]].append(position)
+            #print("match found!!!")
+            match_found = True
+
+
+        
+pp(possible_positions, "count of possible positions", True)            
+pp(number_of_tickets, "number of tickets", True)
+ppp(rule_positions, "rule positions", True, True)
+
+rule_pos_temp = copy.deepcopy(rule_positions)
+rule_positions_final = {}
+solution_found = False
+loop_count = 0
+while loop_count < 20:
+    rule_position = 0
+    for rule in rule_positions:
+        positions = rule_positions[rule]
+        if rule not in rule_positions_final:
+            if len(positions) == 1:
+                rule_position = positions[0]
+                rule_positions_final[rule] = positions[0]
+    #rule_pos_temp = {}
+    for rule in rule_positions:
+        if rule not in rule_positions_final:
+            positions_list = []
+            for position in rule_positions[rule]:
+                if position != rule_position:
+                    positions_list.append(position)
+            rule_positions[rule] = positions_list
+    
+    if len(rule_positions_final) == 19:
+        solution_found = True
+    
+    loop_count += 1
+
+ppp(rule_positions_final, "rule positions solved", True, True)
+pp(myticket, "my ticket", True)
+result = 1
+departure_positions = []
+for key in rule_positions_final:
+    if key.startswith("departure"):
+        departure_positions.append(rule_positions_final[key])
+        result *= int(myticket[rule_positions_final[key]])
+pp(departure_positions, "positions of values named departure", True)
+pp(result, "result", True)
+    
---
+++ advent_2020_ludwig/AOC20/AOC20_17_conway_cubes.py
@@ -0,0 +1,179 @@
+# https://adventofcode.com/2020/day/17
+
+# Each cube only ever considers its neighbors: any of the 26 other 
+# cubes where any of their coordinates differ by at most 1. For 
+# example, given the cube at x=1,y=2,z=3, its neighbors include 
+# the cube at x=2,y=2,z=2, the cube at x=0,y=2,z=3, and so on.
+
+# During a cycle, all cubes simultaneously change their state 
+# according to the following rules:
+
+# If a cube is active and exactly 2 or 3 of its neighbors are 
+# also active, the cube remains active. Otherwise, the cube becomes inactive.
+# If a cube is inactive but exactly 3 of its neighbors are active, 
+# the cube becomes active. Otherwise, the cube remains inactive.
+
+# After the full six-cycle boot process completes, 112 cubes are left 
+# in the active state.
+import copy
+
+data = """.#.
+..#
+###"""
+
+part2 = True
+debug = False
+printit = False
+
+if not debug:
+    data = """.###..#.
+##.##...
+....#.#.
+#..#.###
+...#...#
+##.#...#
+#..##.##
+#......."""
+
+def pp(subject, name = "", override = False): #prints anything with a name as string 
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        #print()
+        print(name, ": ", subject)
+
+
+def ppp(subject, name = "", override = False, levels = 2, isDictionary=False): #prints list entries
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        print()
+        print(name, ": ")
+        if levels == 1:
+            print(subject)
+        elif levels == 2:
+            for item in subject:
+                if isDictionary:
+                    print(item, ":", end="")
+                    print(subject[item])
+            else:
+                print(item)
+        else:
+            for item in subject:
+                print()
+                for item in item:
+                    print(item)
+
+        
+
+print("\n----------------------------------")   # reading file
+
+data = [[ t for t in s] for s in data.split("\n")]
+pp(data, "data")
+gridw, grid, gridx, gridy = [], [], [], []
+
+cycle_count = 7
+
+required_grid_size = len(data[0]) + 2*cycle_count
+required_grid_size_z = 1 + 2*cycle_count
+pp(required_grid_size, "req grid size")
+
+for i in range(required_grid_size):
+    gridx.append(".")
+for i in range(required_grid_size):
+    gridy.append(copy.deepcopy(gridx))
+for i in range(required_grid_size_z):
+    grid.append(copy.deepcopy(gridy))
+if part2:    
+    for i in range(required_grid_size_z):
+        gridw.append(copy.deepcopy(grid))
+
+for index_y, y in enumerate(data):
+    for index_x, x in enumerate(y):
+        if part2:
+            gridw[cycle_count][cycle_count][index_y+cycle_count][index_x+cycle_count] = x
+        else:
+            grid[cycle_count][index_y+cycle_count][index_x+cycle_count] = x
+
+
+def check_neighbors(pos_z, pos_y, pos_x, value, gridtocheck, pos_w, debug=False):
+    neighbors = []
+    active = 0
+    checked = 0
+    if part2:
+        for w in gridtocheck[pos_w-1:pos_w+2]:
+            for z in w[pos_z-1:pos_z+2]:
+                for y in z[pos_y-1:pos_y+2]:
+                    for x in y[pos_x-1:pos_x+2]:
+                        checked += 1
+                        if x == "#":
+                            active += 1
+                        neighbors.append(x)
+    else:
+        for z in gridtocheck[pos_z-1:pos_z+2]:
+            for y in z[pos_y-1:pos_y+2]:
+                for x in y[pos_x-1:pos_x+2]:
+                    checked += 1
+                    if x == "#":
+                        active += 1
+                    neighbors.append(x)
+    #pp(checked, "number of checked neighbors")
+    if value == ".":
+        if active == 3:
+            return "#"
+        else:
+            return "."
+    elif value == "#":
+        active -= 1
+        if active in range(2,4):
+            return "#"
+        else:
+            return "."
+
+cycle = 1
+#ppp(grid,"grid", False, 3)
+while cycle <= (cycle_count):
+    if part2:
+        grid_temp = copy.deepcopy(gridw)
+        for pos_w, w in enumerate(grid_temp):
+            for pos_z, z in enumerate(w):
+                for pos_y, y in enumerate(z):
+                    for pos_x, cube in enumerate(y):
+                        gridw[pos_w][pos_z][pos_y][pos_x] = check_neighbors(pos_z, pos_y, pos_x, cube, grid_temp, pos_w, False)
+    else:
+        grid_temp = copy.deepcopy(grid)
+        for pos_z, z in enumerate(grid_temp):
+            for pos_y, y in enumerate(z):
+                for pos_x, cube in enumerate(y):
+                    grid[pos_z][pos_y][pos_x] = check_neighbors(pos_z, pos_y, pos_x, cube, grid_temp, False, 0)
+    
+    #ppp(grid,"grid", False, 3)
+    actives = 0
+    if part2:
+        for w in gridw:
+            for z in w:
+                for y in z:
+                    for x in y:
+                        if x == "#":
+                            actives += 1
+    else:
+        for z in grid:
+            for y in z:
+                for x in y:
+                    if x == "#":
+                        actives += 1
+    print(f"actives after {cycle} cycles(s): {actives}")
+    cycle += 1
+
+
+
+# Each cube only ever considers its neighbors: any of the 26 other 
+# cubes where any of their coordinates differ by at most 1. For 
+# example, given the cube at x=1,y=2,z=3, its neighbors include 
+# the cube at x=2,y=2,z=2, the cube at x=0,y=2,z=3, and so on.
+
+# During a cycle, all cubes simultaneously change their state 
+# according to the following rules:
+
+# If a cube is active and exactly 2 or 3 of its neighbors are 
+# also active, the cube remains active. Otherwise, the cube becomes inactive.
+# If a cube is inactive but exactly 3 of its neighbors are active, 
+# the cube becomes active. Otherwise, the cube remains inactive.
