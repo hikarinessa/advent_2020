e67d5a77af60c689a6a62bba9bde24c76b76c9f6
kippchenburger <ludwigplath@gmail.com>
2021-02-27 15:17:04 +0100

    day 23 setup

---
+++ advent_2020_ludwig/AOC20/AOC20_23_crabcups.py
@@ -0,0 +1,51 @@
+# https://adventofcode.com/2020/day/23
+
+from copy import deepcopy as deep
+
+debug = True
+printit = False
+part2 = False
+
+def p(text = "", override = False): #prints as string 
+    if debug or printit or override:
+        print(text, flush=True)
+def pp(subject, name = "", override = False): #prints anything with a name as string 
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        #   print()
+        print(name, ": ", subject)
+def ppp(subject, name = "", override = False, isDictionary=False): #prints list entries
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        print()
+        print(name, ": ")
+        for item in subject:
+            if isDictionary:
+                print(item, ":", end="")
+                print(subject[item])
+            else:
+                print(item)
+def pppp(subject, name = "", override = False, isDictionary=False): #prints list's list entries
+    if debug or printit or override:
+        #print("\n", name, ": ", subject)
+        print()
+        print(name, ": ")
+        for item in subject:
+            if isDictionary:
+                print(item, ":", end="")
+                print(subject[item])
+            else:
+                for tile in item:
+                    print()
+                    for line in tile: 
+                        print(line)
+
+print("\n----------------------------------")   # reading file
+
+DATA = "389125467"
+
+if not debug:
+    DATA = "942387615"
+
+_CIRCLE = [int(c) for c in DATA]
+pp(_CIRCLE)
